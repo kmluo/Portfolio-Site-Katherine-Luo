8b355fc4541774ee82380a0393fea64401cdeecb
kmluo <kmluo@berkeley.edu>
2021-02-05 09:27:50 -0800

    Menu Toggle Edit

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ console.log("Hello and welcome to my portfolio site!")
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
